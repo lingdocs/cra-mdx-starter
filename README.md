f819a820df1d5078f1c087ed3dd970f342e52504
lingdocs <71590811+lingdocs@users.noreply.github.com>
2021-10-10 18:45:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Create React App w/ MDX Starter
+
+A boilerplate for Create React App projects w/ MDX import functionality
+
+Bootstrapped with:
+
+- [Create React App](https://reactjs.org/docs/create-a-new-react-app.html)
+- [@lingdocs/mdx-loader](https://github.com/lingdocs/mdx-loader)
+
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// To import mdx files: 
+// /* eslint-disable import/no-webpack-loader-syntax */
+// import * as myDoc from '!babel-loader!@lingdocs/mdx-loader!../pages/index.mdx'
+// const Component = myDoc.default;
+// const { frontMatter } = myDoc;
+// const tableOfContens = myDoc.tableOfContents();
+// use as <Component />
+
+function App() {
+  return (
+    <div className="App">
+      App Here
+    </div>
+  );
+}
+
+export default App;
